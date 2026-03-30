10b5f240898af713ab383a1a2bb73b1721515e16
Alberto Grimaldi <118921715+AlbertoGrimaldi1997@users.noreply.github.com>
2026-03-30 10:31:22 -0500

    Interconnection limits update

---
+++ Core_Models/HydroBoost/src/HydroBoost_Sim.jl
@@ -0,0 +1,66 @@
+#=
+
+HydroBoost Model
+
+Main Authors:
+Jonghwan Kwon; Argonne National Laboratory; kwonj@anl.gov
+Carlos Josue Lopez; Argonne National Laboratory; clopezsalgado@anl.gov
+Alberto Grimaldi; Argonne National Laboratory; agrimaldi@anl.gov
+
+Current version: 3.0
+Last update from Alberto Grimaldi: 03.10.2026
+
+=#
+
+module HydroBoost_Sim
+
+using JuMP
+using MathOptInterface
+using LinearAlgebra
+const _MOI = MathOptInterface
+using CSV
+using FileIO
+
+# Solvers
+#using CPLEX
+using HiGHS
+
+# Create and register module level logger
+using Memento
+const _LOGGER = Memento.getlogger(@__MODULE__)
+__init__() = Memento.register(_LOGGER)
+
+include("core/model_type.jl")
+include("core/base_functions.jl")
+include("core/model_handler.jl")
+
+# Select the model structure by uncommenting the corresponding include line below
+
+# include("model_structure/HydroBoost_Model_A.jl")
+# include("model_structure/HydroBoost_Model_B.jl")
+include("model_structure/HydroBoost_Model_C.jl")
+
+end
+
+# Use the following instructions on the terminal to run the selected model:
+
+### Model A ###
+
+# import Pkg; Pkg.activate("."); Pkg.instantiate()
+# include("Core_Models/HydroBoost/src/HydroBoost_Sim.jl")
+# using .HydroBoost_Sim
+# result = HydroBoost_Sim.execute_HydroBoost_model("Model_A")
+
+### Model B ###
+
+# import Pkg; Pkg.activate("."); Pkg.instantiate()
+# include("Core_Models/HydroBoost/src/HydroBoost_Sim.jl")
+# using .HydroBoost_Sim
+# result = HydroBoost_Sim.execute_HydroBoost_model("Model_B")
+
+### Model C ###
+
+# import Pkg; Pkg.activate("."); Pkg.instantiate()
+# include("Core_Models/HydroBoost/src/HydroBoost_Sim.jl")
+# using .HydroBoost_Sim
+# result = HydroBoost_Sim.execute_HydroBoost_model("Model_C")
